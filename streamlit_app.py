4fec3067215112eee94ab02895bce014a99d0d7c
Richa Rambhia <65843014+rrambhia22@users.noreply.github.com>
2023-12-18 18:33:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,10 @@ import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
 
-streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 & Blueberry Oatmeal")
-streamlit.text("Kale, Spinach & Rocket Smoothie")
-streamlit.text("Hard-Bolied Free-Range Egg")
+streamlit.header("Breakfast Favorites")
+streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔 Hard-Bolied Free-Range Egg")
+streamlit.text("🥑🍞 Avacoda Toast")
 
 
