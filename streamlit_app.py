8af8e95bbbc7812ea02c6bf44cb12f5773dd7f32
Richa Rambhia <65843014+rrambhia22@users.noreply.github.com>
2023-12-18 18:36:31 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,5 @@ streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔 Hard-Bolied Free-Range Egg")
 streamlit.text("🥑🍞 Avacoda Toast")
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
 
