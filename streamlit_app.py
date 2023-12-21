edb278d9cb7d0cb0a93e53ff33023f98eb716077
Richa Rambhia <65843014+rrambhia22@users.noreply.github.com>
2023-12-20 21:33:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ streamlit.text("🥑🍞 Avacoda Toast")
 streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
 
 import pandas
-
+import snowflake.connector
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index("Fruit")
